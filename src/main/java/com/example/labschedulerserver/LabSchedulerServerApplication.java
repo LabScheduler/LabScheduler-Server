671719130a29cce9c08e9fe9782508b3107f53bf
toanehihi <hocongtoan987@gmail.com>
2025-01-06 22:28:06 +0700

    initial commit

---
+++ src/main/java/com/example/labschedulerserver/LabSchedulerServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.labschedulerserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabSchedulerServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabSchedulerServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/labschedulerserver/LabSchedulerServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.labschedulerserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabSchedulerServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
