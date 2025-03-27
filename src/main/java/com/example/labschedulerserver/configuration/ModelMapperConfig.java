a65cc38f70236698214720e2e8615dbb1a4cafd5
toanehihi <hocongtoan987@gmail.com>
2025-03-27 16:28:02 +0700

    modelmapper config

--- src/main/java/com/example/labschedulerserver/configuration/ModelMapperConfig.java
+++ src/main/java/com/example/labschedulerserver/configuration/ModelMapperConfig.java
@@ -1,4 +1,19 @@
 package com.example.labschedulerserver.configuration;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ModelMapperConfig {
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration()
+                .setMatchingStrategy(MatchingStrategies.STRICT)
+                .setFieldMatchingEnabled(true)
+                .setSkipNullEnabled(true);
+        return modelMapper;
+    }
 }
