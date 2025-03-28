a552a453cb89d9102a83df0ffbc6104dc72349b0
toanehihi <hocongtoan987@gmail.com>
2025-03-28 11:33:50 +0700

    refactor room service

--- src/main/java/com/example/labschedulerserver/service/implement/RoomServiceImpl.java
+++ src/main/java/com/example/labschedulerserver/service/implement/RoomServiceImpl.java
@@ -1,17 +1,17 @@
 package com.example.labschedulerserver.service.implement;
 
 import com.example.labschedulerserver.common.RoomStatus;
+import com.example.labschedulerserver.exception.FieldNotFoundException;
+import com.example.labschedulerserver.exception.ResourceNotFoundException;
 import com.example.labschedulerserver.model.Room;
 import com.example.labschedulerserver.payload.request.AddRoomRequest;
 import com.example.labschedulerserver.repository.RoomRepository;
 import com.example.labschedulerserver.service.RoomService;
 import com.example.labschedulerserver.ultils.ConvertFromJsonToTypeVariable;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 import java.lang.reflect.Field;
-import java.sql.Timestamp;
 import java.util.List;
 import java.util.Map;
 
@@ -27,7 +27,7 @@ public class RoomServiceImpl implements RoomService {
 
     @Override
     public Room getRoomById(Long id) {
-        return roomRepository.findById(id).orElseThrow(() -> new RuntimeException("Room not found with id: " + id));
+        return roomRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Room not found with id: " + id));
     }
 
     @Override
@@ -44,13 +44,14 @@ public class RoomServiceImpl implements RoomService {
 
     @Override
     public void deleteRoomById(Long id) {
-        Room room = roomRepository.findById(id).orElseThrow(() -> new RuntimeException("Room not found with id: " + id));
+        Room room = roomRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Room not found with id: " + id));
         roomRepository.delete(room);
     }
 
     @Override
     public Room updateRoomById(Long id, Map<String, Object> payload) {
-        Room tmpRoom = roomRepository.findById(id).orElseThrow(() -> new RuntimeException("Room not found with id: " + id));
+        Room tmpRoom = roomRepository.findById(id)
+                .orElseThrow(() -> new ResourceNotFoundException("Room not found with id: " + id));
 
         for (Map.Entry<String, Object> entry : payload.entrySet()) {
             String key = entry.getKey();
@@ -58,6 +59,7 @@ public class RoomServiceImpl implements RoomService {
             try {
                 Field field = Room.class.getDeclaredField(ConvertFromJsonToTypeVariable.convert(key));
                 field.setAccessible(true);
+
                 if (field.getType().isEnum()) {
                     Object enumValue = Enum.valueOf((Class<Enum>) field.getType(), value.toString());
                     field.set(tmpRoom, enumValue);
@@ -65,10 +67,9 @@ public class RoomServiceImpl implements RoomService {
                     field.set(tmpRoom, value);
                 }
             } catch (NoSuchFieldException | IllegalAccessException e) {
-                throw new RuntimeException("Field not found");
+                throw new FieldNotFoundException("Field not found: " + key, e);
             }
         }
-        roomRepository.save(tmpRoom);
-        return tmpRoom;
+        return roomRepository.save(tmpRoom);
     }
 }
