c4957a4dd83f98201384d0df619b10748fd16693
toanehihi <hocongtoan987@gmail.com>
2025-03-28 11:34:14 +0700

    add modelmapper

--- src/main/java/com/example/labschedulerserver/configuration/ModelMapperConfig.java
+++ src/main/java/com/example/labschedulerserver/configuration/ModelMapperConfig.java
@@ -1,5 +1,7 @@
 package com.example.labschedulerserver.configuration;
 
+import com.example.labschedulerserver.model.Course;
+import com.example.labschedulerserver.payload.response.CourseResponse;
 import org.modelmapper.ModelMapper;
 import org.modelmapper.convention.MatchingStrategies;
 import org.springframework.context.annotation.Bean;
@@ -14,6 +16,15 @@ public class ModelMapperConfig {
                 .setMatchingStrategy(MatchingStrategies.STRICT)
                 .setFieldMatchingEnabled(true)
                 .setSkipNullEnabled(true);
+
+        modelMapper.typeMap(Course.class, CourseResponse.class)
+                .addMapping(course ->course.getSubject().getName(), CourseResponse::setSubject)
+                .addMapping(course -> course.getClazz().getName(), CourseResponse::setClazz)
+                .addMapping(course -> course.getSemester().getName(), CourseResponse::setSemester)
+                .addMapping(course -> course.getLecturerAccount().getFullName(), CourseResponse::setLecturer)
+                .addMapping(course -> course.getId(), CourseResponse::setId);
+
+
         return modelMapper;
     }
 }
