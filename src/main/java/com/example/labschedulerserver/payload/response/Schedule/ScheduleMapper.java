7fa47ef7beeb6330ec723aa8005d453bb5d80a58
toanehihi <hocongtoan987@gmail.com>
2025-04-03 01:47:47 +0700

    schedule mapper

--- src/main/java/com/example/labschedulerserver/payload/response/Schedule/ScheduleMapper.java
+++ src/main/java/com/example/labschedulerserver/payload/response/Schedule/ScheduleMapper.java
@@ -1,4 +1,24 @@
 package com.example.labschedulerserver.payload.response.Schedule;
 
+import com.example.labschedulerserver.model.Schedule;
+
 public class ScheduleMapper {
+    public static ScheduleResponse mapScheduleToResponse(Schedule schedule) {
+        return ScheduleResponse.builder()
+                .id(schedule.getId())
+                .subjectCode(schedule.getCourse().getSubject().getCode())
+                .subjectName(schedule.getCourse().getSubject().getName())
+                .courseGroup(schedule.getCourse().getGroupNumber())
+                .courseSection(schedule.getCourseSection().getSectionNumber())
+                .room(schedule.getRoom().getName())
+                .dayOfWeek(schedule.getDayOfWeek())
+                .startPeriod(schedule.getStartPeriod())
+                .totalPeriod(schedule.getTotalPeriod())
+                .lecturer(schedule.getCourse().getLecturerAccount().getFullName())
+                .type(schedule.getScheduleType().name())
+                .semesterWeek(schedule.getSemesterWeek().getName())
+                .studyDate(schedule.getStudyDate())
+                .status(schedule.getScheduleStatus().name())
+                .build();
+    }
 }
--- src/main/java/com/example/labschedulerserver/payload/response/Schedule/ScheduleResponse.java
+++ src/main/java/com/example/labschedulerserver/payload/response/Schedule/ScheduleResponse.java
@@ -1,4 +1,48 @@
-package com.example.labschedulerserver.payload.response;
+package com.example.labschedulerserver.payload.response.Schedule;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Builder;
+import lombok.Data;
+
+import java.sql.Timestamp;
+
+@Data
+@Builder
 public class ScheduleResponse {
+    private Long id;
+
+    @JsonProperty("subject_code")
+    private String subjectCode;
+
+    @JsonProperty("subject_name")
+    private String subjectName;
+
+    @JsonProperty("course_group")
+    private Integer courseGroup;
+
+    @JsonProperty("course_section")
+    private Integer courseSection;
+
+    private String room;
+
+    @JsonProperty("day_of_week")
+    private Byte dayOfWeek;
+
+    @JsonProperty("start_period")
+    private Byte startPeriod;
+
+    @JsonProperty("total_period")
+    private Byte totalPeriod;
+
+    private String lecturer;
+
+    private String type;
+
+    @JsonProperty("semester_week")
+    private String semesterWeek;
+
+    @JsonProperty("study_date")
+    private Timestamp studyDate;
+
+    private String status;
 }
