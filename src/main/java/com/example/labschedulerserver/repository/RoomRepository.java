f95f5357dd16dbf710f7f75c635bc9404f011ae0
toanehihi <hocongtoan987@gmail.com>
2025-04-03 20:12:46 +0700

    chore

--- src/main/java/com/example/labschedulerserver/repository/RoomRepository.java
+++ src/main/java/com/example/labschedulerserver/repository/RoomRepository.java
@@ -1,5 +1,6 @@
 package com.example.labschedulerserver.repository;
 
+import com.example.labschedulerserver.common.RoomStatus;
 import com.example.labschedulerserver.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -9,4 +10,6 @@ import java.util.Optional;
 @Repository
 public interface RoomRepository extends JpaRepository<Room, Long> {
     Optional<Room> findByName(String name);
+
+    Optional<Room> findAllByStatus(RoomStatus status);
 }
