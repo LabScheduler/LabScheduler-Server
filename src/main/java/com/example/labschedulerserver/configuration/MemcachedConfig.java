f29a2fa1c3edfdd19709b5493b1fb035aa271255
toanehihi <hocongtoan987@gmail.com>
2025-04-06 10:49:46 +0700

    memcache config

--- src/main/java/com/example/labschedulerserver/configuration/MemcachedConfig.java
+++ src/main/java/com/example/labschedulerserver/configuration/MemcachedConfig.java
@@ -1,6 +1,7 @@
 package com.example.labschedulerserver.configuration;
 
 import net.spy.memcached.MemcachedClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,9 +10,12 @@ import java.net.InetSocketAddress;
 
 @Configuration
 public class MemcachedConfig {
+
+    @Value("${spring.memcached.host}")
+    private String memcachedHost;
+
     @Bean
     public MemcachedClient memcachedClient() throws IOException {
-        return new MemcachedClient(new InetSocketAddress("localhost", 11211));
+        return new MemcachedClient(new InetSocketAddress(memcachedHost, 11211));
     }
-
 }
