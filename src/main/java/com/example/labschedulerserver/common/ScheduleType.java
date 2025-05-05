9734cfdd8d60e4e1f819c89aab6e155d4da2674f
toanehihi <hocongtoan987@gmail.com>
2025-05-05 16:06:30 +0700

    WIP

---
+++ src/main/java/com/example/labschedulerserver/common/ScheduleType.java
@@ -0,0 +1,4 @@
+package com.example.labschedulerserver.common;
+
+public enum ScheduleType {
+}
---
+++ src/main/java/com/example/labschedulerserver/service/implement/ScheduleServiceImpl.java
@@ -0,0 +1,4 @@
+package com.example.labschedulerserver.service.implement;
+
+public class ScheduleServiceImpl {
+}
